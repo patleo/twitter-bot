5d4733750a304266c3b4bcd4a23b4fbc786999d3
Patrick Sweeney <plsweeney25@gmail.com>
2016-05-11 16:02:18 -0400

    basic set up

---
+++ bot.py
@@ -0,0 +1,9 @@
+from TwitterAPI import TwitterAPI
+import setup
+
+api = TwitterAPI(setup.consumer_key, setup.consumer_secret, setup.access_token_key, setup.access_token_secret)
+
+r = api.request('users/show', {'screen_name':'walmart'})
+
+for item in r:
+    print item['followers_count']
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os, sys
+
+local = False # Default
+
+if 'DYNO' not in os.environ:
+    local = True
+    
+if local == False:
+    consumer_key = os.environ['consumer_key']
+    consumer_secret = os.environ['consumer_secret']
+    access_token_key = os.environ['access_token_key']
+    access_token_secret = os.environ['access_token_secret']
+    
+else:
+    import localsetup
+    consumer_key = localsetup.consumer_key 
+    consumer_secret = localsetup.consumer_secret
+    access_token_key = localsetup.access_token_key
+    access_token_secret = localsetup.access_token_secret
