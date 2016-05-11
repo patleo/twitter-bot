b1e785335abd43c73224bc0113fa5becb7cf70a4
Patrick Sweeney <plsweeney25@gmail.com>
2016-05-11 17:26:40 -0400

    added in quotes for strings in sqlstatement

--- dbwrapper.py
+++ dbwrapper.py
@@ -19,7 +19,7 @@ class DBWrapper:
         cur = self.conn.cursor()
         cur.execute('CREATE TABLE Companies (id INTEGER PRIMARY KEY, Twitter varchar(255), Ticker varchar(255), Rank int);')
         for i in range(len(twitter_handle)):
-            comm = "INSERT INTO Companies (id, Twitter, Ticker, Rank) VALUES({},{},{},{})".format(i+1 ,twitter_handle[i], stock_ticker[i], forbes_rank[i])
+            comm = "INSERT INTO Companies (id, Twitter, Ticker, Rank) VALUES({},'{}','{}',{})".format(i+1 ,twitter_handle[i], stock_ticker[i], forbes_rank[i])
             cur.execute(comm)
             
         self.conn.commit()
