7f5bdb0944b0d92effa6dc7f1f99ef22c12747f1
Patrick Sweeney <plsweeney25@gmail.com>
2016-05-11 17:24:08 -0400

    added in column names in seeding process

--- dbwrapper.py
+++ dbwrapper.py
@@ -17,9 +17,9 @@ class DBWrapper:
         
     def seed_companies_table(self, twitter_handle, stock_ticker, forbes_rank):
         cur = self.conn.cursor()
-        cur.execute('CREATE TABLE Companies (Id INTEGER PRIMARY KEY, Twitter varchar(255), Ticker varchar(255), Rank int);')
+        cur.execute('CREATE TABLE Companies (id INTEGER PRIMARY KEY, Twitter varchar(255), Ticker varchar(255), Rank int);')
         for i in range(len(twitter_handle)):
-            comm = "INSERT INTO Companies VALUES({},{},{})".format(i+1 ,twitter_handle[i], stock_ticker[i], forbes_rank[i])
+            comm = "INSERT INTO Companies (id, Twitter, Ticker, Rank) VALUES({},{},{},{})".format(i+1 ,twitter_handle[i], stock_ticker[i], forbes_rank[i])
             cur.execute(comm)
             
         self.conn.commit()
