758efc1907dabdd3748d96c9578e61522c1326a7
Patrick Sweeney <plsweeney25@gmail.com>
2016-06-04 23:11:40 -0400

    updated bot from collecting data on fortune 500 company followers to one that generates tweets w/o any NLP

--- bot.py
+++ bot.py
@@ -1,47 +1,39 @@
-import sys
 from TwitterAPI import TwitterAPI
 import setup
-from dbwrapper import DBWrapper
-from twitterclasses import Company
-
-
-db = DBWrapper()
-
-if len(sys.argv) > 1:
-    for arg in sys.argv[1:]:
-        if arg == 'print-twitter':
-            db.print_table('Twitter')
-            db.close_conn()
-            sys.exit(0)
-
-company_table = db.return_table('Companies')
-company_list = []
-
-# Returns company info from Company table and adds necessary info to list of companies
-for comp in company_table:
-    c = Company(comp[1], comp[2]) 
-    company_list.append(c)
+from twitterclasses import Markov, TweetProc
+import random
 
 # Sets up Twitter auth    
 api = TwitterAPI(setup.consumer_key, setup.consumer_secret, setup.access_token_key, setup.access_token_secret)  
 
-# Goes through list of companies and makes twitter api request to return followers count
-for comp in company_list:
-    r = api.request('users/show', {'screen_name': comp.twitter_handle})
-    for item in r.get_iterator():
-        comp.followers_count = item['followers_count']
-        if 'text' in item:
-            print item['text']
-        elif 'message' in item:
-            print '%s (%d)' % (item['message'], item['code'])
+min_volume = 1000
+topics = []
+r = api.request('trends/place', {'id': '2357024'})
+for item in r.get_iterator():
+    if item['tweet_volume'] > min_volume:
+        topics.append(item['name'])
+    if 'text' in item:
+        print item['text']
+    elif 'message' in item:
+        print '%s (%d)' % (item['message'], item['code'])
 
-# Creates two lists to feed the Twitter table
-twitter_handles = []
-twitter_followers = []
-
-for comp in company_list:
-    twitter_handles.append(comp.twitter_handle)
-    twitter_followers.append(comp.followers_count)
+tweet_text = ''
+r = api.request('search/tweets', {'q': random.choice(topics), 'count': '100'})
+for item in r.get_iterator():
+    tweet_text += item['text']
     
-db.add_twitter_row(twitter_handles, twitter_followers)
-db.close_conn()
+mark = Markov(tweet_text)
+proc = TweetProc(tweet_text)
+tweet = ''
+
+for x in range(1):
+    tweet = mark.generate_tweet_text()
+    while not proc.original(tweet) and proc.too_long(tweet):
+        tweet = mark.generate_tweet_text()
+    tweet = proc.remove_mentions(tweet)
+    
+
+print str(proc.long_count)
+print str(proc.dupe_count)
+
+api.request('statuses/update', {'status': tweet})
--- twitterclasses.py
+++ twitterclasses.py
@@ -1,7 +1,66 @@
-class Company:
-    
-    def __init__(self, twitter, ticker):
+import random
+
+class Markov: 
+    def __init__(self, text):
+        self.word_dict = {}
+        self.words = text.lower().split()
+        self.words_len = len(self.words)
+        self.generate_dict()
         
-        self.twitter_handle = twitter
-        self.stock_ticker = ticker
-        # self.followers_count
+    def thirds(self):
+        for index in range(len(self.words) - 2):
+            yield (self.words[index], self.words[index+1], self.words[index+2])
+            
+    def generate_dict(self):
+        
+        for w1, w2, w3 in self.thirds():
+            key = (w1, w2)
+            if key in self.word_dict:
+                self.word_dict[key].append(w3)
+            else:
+                self.word_dict[key] = [w3]
+
+    def generate_tweet_text(self, word_limit=20):
+        
+        n = random.randint(0, self.words_len - (word_limit + 3))
+        w1, w2 = self.words[n], self.words[n + 1]
+        tweet = ''
+        
+        for x in xrange(word_limit):
+            tweet += '%s ' % (w1)
+            try:
+                w1, w2 =  w2, random.choice(self.word_dict[(w1, w2)])
+            except KeyError:
+                print "EXCEPTION"
+                n = random.randint(0, self.words_len - (word_limit + 3 - x))
+                w1, w2 = self.words[n], self.words[n + 1]
+        return tweet.capitalize()
+
+class TweetProc:
+    def __init__(self, text):
+        self.text = text
+        self.dupe_count = 0
+        self.long_count = 0
+        
+    def original(self,tweet):
+        if self.text.lower().find(tweet.lower()) > -1:
+            print "NOT ORIGINAL"
+            self.dupe_count += 1
+            return False
+        return True
+    def too_long(self, tweet):
+        if len(tweet) > int(140):
+            print "TOO LONG"
+            self.long_count += 1
+    def remove_mentions(self, tweet):
+        words = tweet.split()
+        retVal = ''
+        for word in words[:]:
+            if word.find('@') == 0:
+                words.remove(word)
+        for word in words:
+            print word
+            retVal += '%s ' % (word)
+        return retVal
+        
+    
